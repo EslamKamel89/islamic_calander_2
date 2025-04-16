c6581637421b90bf4bbe049c052474bc6befdb37
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-16 17:05:51 +0200

    feat: Integrate Firebase services into the Islamic Calendar app
    - Added Firebase configuration to Android build files (build.gradle, settings.gradle).
    - Created google-services.json for Firebase project setup.
    - Generated firebase_options.dart for Firebase initialization.
    - Updated pubspec.yaml to include Firebase dependencies (firebase_auth, firebase_core, firebase_messaging).
    - Modified GeneratedPluginRegistrant.swift to register Firebase plugins for macOS.
    - Updated generated_plugin_registrant.cc to register Firebase plugins for Windows.
    - Added generated_plugins.cmake to include Firebase plugins in the build process.
    - Created fire.txt and firebase.json for Firebase app configuration details.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,9 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
+import firebase_core
+import firebase_messaging
 import flutter_timezone
 import geolocator_apple
 import path_provider_foundation
@@ -13,6 +16,9 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
