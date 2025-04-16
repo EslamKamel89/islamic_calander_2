1844b185eafbbc4ab020b31fb376bc47d7c7c66f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-16 17:06:17 +0200

    feat: Initialize Firebase in main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,6 +16,7 @@ import 'package:islamic_calander_2/features/date_info/presentation/cubits/eclips
 import 'package:islamic_calander_2/features/date_info/presentation/cubits/moon_phase/moon_phase_cubit.dart';
 import 'package:islamic_calander_2/features/main_homepage/cubits/moon_image/moon_image_cubit.dart';
 import 'package:islamic_calander_2/features/main_homepage/cubits/update_next_prayer_api/update_next_prayer_api_cubit.dart';
+import 'package:islamic_calander_2/firebase_options.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
@@ -23,6 +25,9 @@ void main() async {
   await findSystemLocale();
   await EasyLocalization.ensureInitialized();
   tz.initializeTimeZones();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
@@ -50,8 +55,7 @@ class MyApp extends StatelessWidget {
             create: (_) => ThemeCubit(),
           ),
           BlocProvider(
-            create: (context) =>
-                DateConversionCubit(dateConversionRepo: serviceLocator()),
+            create: (context) => DateConversionCubit(dateConversionRepo: serviceLocator()),
           ),
           BlocProvider(
             create: (context) => DateYearCubit(dateInfoRepo: serviceLocator()),
