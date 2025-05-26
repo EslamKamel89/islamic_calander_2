a76631ce70be244bdd15ba9df2621d50ff988458
EslamKamel89 <eslam_kamel_89@hotmail.com>
2025-05-26 15:05:04 +0300

    feat: update app name to 'Eternal Islamic Calendar' and bump version to 1.0.37; enhance home widget functionality

--- lib/features/main_homepage/presentation/widgets/next_prayer_widget.dart
+++ lib/features/main_homepage/presentation/widgets/next_prayer_widget.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:islamic_calander_2/core/heleprs/prayer_name_tr.dart';
+import 'package:islamic_calander_2/features/home_widget/controller/home_widget_controller.dart';
 import 'package:islamic_calander_2/features/main_homepage/cubits/update_next_prayer_api/update_next_prayer_api_cubit.dart';
 import 'package:islamic_calander_2/features/main_homepage/presentation/widgets/prayer_card.dart';
 
@@ -25,13 +26,22 @@ class _NextPrayerWidgetState extends State<NextPrayerWidget> {
         //       .animate(onPlay: (controller) => controller.repeat)
         //       .fade(duration: 500.ms, begin: 0, end: 0.5);
         // }
+        HomeWidgetController.updateNextPrayer(state.nextPrayer, _formateTimeRemaining(state.timeRemaining));
         return PrayerCard(
-          prayerName: state.nextPrayer != null
-              ? prayerNameTr(state.nextPrayer!)
-              : 'CALC'.tr(),
+          prayerName: state.nextPrayer != null ? prayerNameTr(state.nextPrayer!) : 'CALC'.tr(),
           timeRemaining: state.timeRemaining ?? const Duration(seconds: 0),
         );
       },
     );
+  }
+
+  String _formateTimeRemaining(Duration? timeRemaining) {
+    if (timeRemaining == null) return '00:00';
+    final hours = timeRemaining.inHours;
+    final minutes = timeRemaining.inMinutes.remainder(60);
+    final seconds = timeRemaining.inSeconds.remainder(60);
+    return '${hours.toString().padLeft(2, '0')}:'
+        '${minutes.toString().padLeft(2, '0')}:'
+        '${seconds.toString().padLeft(2, '0')}';
   }
 }
