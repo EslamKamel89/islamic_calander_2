417014fa71b3b2511b33e8a56886861cf878e177
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-04 14:10:07 +0200

    feat(about): add mode field to AboutModel and parse it from API response

--- lib/features/about/models/about_model.dart
+++ lib/features/about/models/about_model.dart
@@ -3,11 +3,12 @@ import 'package:islamic_calander_2/core/heleprs/print_helper.dart';
 class AboutModel {
   String? ar;
   String? en;
+  String? mode;
 
-  AboutModel({this.ar, this.en});
+  AboutModel({this.ar, this.en, this.mode});
 
   @override
-  String toString() => 'AboutModel(ar: $ar, en: $en)';
+  String toString() => 'AboutModel(mode: $mode, ar: $ar, en: $en)';
 
   factory AboutModel.fromJson(Map<String, dynamic> json) {
     pr(json, 'json inside AboutModel');
@@ -15,21 +16,25 @@ class AboutModel {
       ar: (json['ar'] as String?),
       // ?.replaceAll('line-height: 50px;', ''),
       en: json['en'] as String?,
+      mode: json['mode']?.toString(),
     );
   }
 
   Map<String, dynamic> toJson() => {
         'ar': ar,
         'en': en,
+        'model': mode,
       };
 
   AboutModel copyWith({
     String? ar,
     String? en,
+    String? mode,
   }) {
     return AboutModel(
       ar: ar ?? this.ar,
       en: en ?? this.en,
+      mode: mode ?? this.mode,
     );
   }
 }
