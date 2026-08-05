264493f478d3b97e0e37a77bf094a3a9884cc01a
EslamKamel89 <elsamkamelforex@gmail.com>
2026-08-05 13:15:33 +0300

    perf(prayer-times): add in-memory caching with request deduplication

--- lib/features/main_homepage/controllers/params.dart
+++ lib/features/main_homepage/controllers/params.dart
@@ -1,4 +1,6 @@
 // https://api.aladhan.com/v1/timings/01-01-2025?latitude=51.5194682&longitude=-0.1360365&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ
+import 'package:islamic_calander_2/core/extensions/datatime_extension.dart';
+
 class PrayerTimeParams {
   double? latitude;
   double? longitude;
@@ -6,11 +8,7 @@ class PrayerTimeParams {
   DateTime? date;
   LatitudeAdjustmentMethod? latitudeAdjustmentMethod;
   PrayerTimeParams(
-      {this.latitude,
-      this.longitude,
-      this.method,
-      this.latitudeAdjustmentMethod,
-      this.date});
+      {this.latitude, this.longitude, this.method, this.latitudeAdjustmentMethod, this.date});
 
   PrayerTimeParams copyWith({
     double? latitude,
@@ -23,8 +21,7 @@ class PrayerTimeParams {
       latitude: latitude ?? this.latitude,
       longitude: longitude ?? this.longitude,
       method: method ?? this.method,
-      latitudeAdjustmentMethod:
-          latitudeAdjustmentMethod ?? this.latitudeAdjustmentMethod,
+      latitudeAdjustmentMethod: latitudeAdjustmentMethod ?? this.latitudeAdjustmentMethod,
       date: date ?? this.date,
     );
   }
@@ -38,6 +35,11 @@ class PrayerTimeParams {
     };
   }
 
+  String toKey(DateTime date) {
+    final d = date.toShortDateString();
+    return '$latitude/$longitude/$method/$latitudeAdjustmentMethod/$d';
+  }
+
   @override
   String toString() {
     return 'PrayerTimeParams(latitude: $latitude, longitude: $longitude, method: $method, latitudeAdjustmentMethod: $latitudeAdjustmentMethod)';
@@ -47,47 +49,40 @@ class PrayerTimeParams {
 enum IslamicOrganization {
   // jafariShiaIthnaAshari(0, 'Jafari / Shia Ithna-Ashari'),
   auto(1, 'auto', "اوتو"),
-  universityIslamicSciencesKarachi(1, 'University of Islamic Sciences, Karachi',
-      "جامعة العلوم الإسلامية، كراتشي"),
-  islamicSocietyNorthAmerica(2, 'Islamic Society of North America',
-      "الجمعية الإسلامية لأمريكا الشمالية"),
+  universityIslamicSciencesKarachi(
+      1, 'University of Islamic Sciences, Karachi', "جامعة العلوم الإسلامية، كراتشي"),
+  islamicSocietyNorthAmerica(
+      2, 'Islamic Society of North America', "الجمعية الإسلامية لأمريكا الشمالية"),
   muslimWorldLeague(3, 'Muslim World League', "رابطة العالم الإسلامي"),
-  ummAlQuraUniversity(
-      4, 'Umm Al-Qura University, Makkah', "جامعة أم القرى، مكة"),
-  egyptianGeneralAuthority(5, 'Egyptian General Authority of Survey',
-      "الهيئة المصرية العامة للمساحة"),
-  instituteOfGeophysics(7, 'Institute of Geophysics, University of Tehran',
-      "معهد الجيوفيزياء، جامعة طهران"),
+  ummAlQuraUniversity(4, 'Umm Al-Qura University, Makkah', "جامعة أم القرى، مكة"),
+  egyptianGeneralAuthority(
+      5, 'Egyptian General Authority of Survey', "الهيئة المصرية العامة للمساحة"),
+  instituteOfGeophysics(
+      7, 'Institute of Geophysics, University of Tehran', "معهد الجيوفيزياء، جامعة طهران"),
   gulfRegion(8, 'Gulf Region', "منطقة الخليج"),
   kuwait(9, 'Kuwait', "الكويت"),
   qatar(10, 'Qatar', "قطر"),
-  majlisUgamaIslamSingapura(11, 'Majlis Ugama Islam Singapura, Singapore',
-      "مجلس العلماء المسلمين، سنغافورة"),
+  majlisUgamaIslamSingapura(
+      11, 'Majlis Ugama Islam Singapura, Singapore', "مجلس العلماء المسلمين، سنغافورة"),
   unionOrganizationIslamicDeFrance(
       12, 'Union Organization islamic de France', "المنظمة الإسلامية الفرنسية"),
-  diyanetIsleri(
-      13, 'Diyanet İşleri Başkanlığı, Turkey', "رئاسة الشؤون الدينية التركية"),
+  diyanetIsleri(13, 'Diyanet İşleri Başkanlığı, Turkey', "رئاسة الشؤون الدينية التركية"),
   spiritualAdministrationMuslimsRussia(
-      14,
-      'Spiritual Administration of Muslims of Russia',
-      "الإدارة الدينية لمسلمي روسيا"),
+      14, 'Spiritual Administration of Muslims of Russia', "الإدارة الدينية لمسلمي روسيا"),
   moonsightingCommitteeWorldwide(
       15,
       'Moonsighting Committee Worldwide (also requires shafaq parameter)',
       "لجنة رؤية الهلال العالمية (يتطلب أيضا معلمة الشفق)"),
   dubai(16, 'Dubai (experimental)', "دبي (تجريبي)"),
-  jabatanKemajuanIslamMalaysia(17, 'Jabatan Kemajuan Islam Malaysia (JAKIM)',
-      "إدارة التنمية الإسلامية الماليزية"),
+  jabatanKemajuanIslamMalaysia(
+      17, 'Jabatan Kemajuan Islam Malaysia (JAKIM)', "إدارة التنمية الإسلامية الماليزية"),
   tunisia(18, 'Tunisia', "تونس"),
   algeria(19, 'Algeria', "الجزائر"),
   kemenagIndonesia(20, 'KEMENAG - Kementerian Agama Republik Indonesia',
       "وزارة الشؤون الدينية للجمهورية الإندونيسية"),
   morocco(21, 'Morocco', "المغرب"),
-  comunidadeIslamicaLisboa(
-      22, 'Comunidade Islamica de Lisboa', "الجماعة الإسلامية في لشبونة"),
-  ministryAwqafJordan(
-      23,
-      'Ministry of Awqaf, Islamic Affairs and Holy Places, Jordan',
+  comunidadeIslamicaLisboa(22, 'Comunidade Islamica de Lisboa', "الجماعة الإسلامية في لشبونة"),
+  ministryAwqafJordan(23, 'Ministry of Awqaf, Islamic Affairs and Holy Places, Jordan',
       "وزارة الأوقاف والشؤون والمقدسات الإسلامية، الأردن");
   // custom(99, 'Custom');
 
--- lib/features/main_homepage/controllers/prayers_controller.dart
+++ lib/features/main_homepage/controllers/prayers_controller.dart
@@ -1,53 +1,101 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:islamic_calander_2/core/api_service/api_consumer.dart';
 import 'package:islamic_calander_2/core/api_service/end_points.dart';
 import 'package:islamic_calander_2/core/enums/response_state.dart';
 import 'package:islamic_calander_2/core/globals/calc_method_settings.dart';
-import 'package:islamic_calander_2/core/globals/globals_var.dart';
 import 'package:islamic_calander_2/core/heleprs/format_date.dart';
 import 'package:islamic_calander_2/core/heleprs/print_helper.dart';
-import 'package:islamic_calander_2/core/heleprs/snackbar.dart';
 import 'package:islamic_calander_2/core/models/api_response_model.dart';
 import 'package:islamic_calander_2/core/service_locator/service_locator.dart';
 import 'package:islamic_calander_2/features/main_homepage/controllers/params.dart';
 import 'package:islamic_calander_2/features/main_homepage/models/prayers_time_model.dart';
 
+final Map<String, ApiResponseModel<PrayersTimeModel>> _cache = {};
+final Map<String, Future<ApiResponseModel<PrayersTimeModel>>> _inFlight = {};
+
 class PrayersController {
-  ApiConsumer api = serviceLocator();
+  final ApiConsumer api = serviceLocator();
+
   Future<ApiResponseModel<PrayersTimeModel>> prayerTime(PrayerTimeParams params) async {
-    final t = prt('prayerTime - PrayersControllers');
+    final t = prt('prayerTime - PrayersController');
+
     if (params.method == IslamicOrganization.auto) {
       params.method =
           (await getPrayerCalcMethodByPosition()) ?? IslamicOrganization.muslimWorldLeague;
     } else {
       params.method = selectedPrayersMethod;
     }
+
+    final date = params.date ?? DateTime.now();
+    final key = params.toKey(date);
+
+    final cached = _cache[key];
+    if (cached != null) {
+      return pr(cached, '$t (memory cache)');
+    }
+
+    final running = _inFlight[key];
+    if (running != null) {
+      return await running;
+    }
+
+    final future = _fetchPrayerTime(params, date, key, t);
+
+    _inFlight[key] = future;
+
+    return await future;
+  }
+
+  Future<ApiResponseModel<PrayersTimeModel>> _fetchPrayerTime(
+    PrayerTimeParams params,
+    DateTime date,
+    String key,
+    String t,
+  ) async {
     try {
       final response = await api.get(
-        EndPoint.prayerTimesEndPoint(params.date ?? DateTime.now()),
+        EndPoint.prayerTimesEndPoint(date),
         queryParameter: params.toMap(),
       );
-      return pr(
-          ApiResponseModel(
-            response: ResponseEnum.success,
-            data: PrayersTimeModel.fromJson(response['data']['timings'],
-                date: formatDateForApi(params.date ?? DateTime.now())),
+
+      final result = pr(
+        ApiResponseModel(
+          response: ResponseEnum.success,
+          data: PrayersTimeModel.fromJson(
+            response['data']['timings'],
+            date: formatDateForApi(date),
           ),
-          t);
+        ),
+        t,
+      );
+
+      _cache[key] = result;
+
+      return result;
     } catch (e) {
       String errorMessage = e.toString();
+
       if (e is DioException) {
-        errorMessage = jsonEncode(e.response?.data ?? 'Unknown error occured');
+        errorMessage = jsonEncode(e.response?.data ?? 'Unknown error occurred');
       }
-      BuildContext? context = navigatorKey.currentContext;
-      if (context != null) {
-        showSnackbar('Error', errorMessage, true);
-      }
-      return pr(ApiResponseModel(errorMessage: errorMessage, response: ResponseEnum.failure), t);
+
+      // final context = navigatorKey.currentContext;
+
+      // if (context != null) {
+      //   showSnackbar('Error', errorMessage, true);
+      // }
+
+      return pr(
+        ApiResponseModel(
+          errorMessage: errorMessage,
+          response: ResponseEnum.failure,
+        ),
+        t,
+      );
+    } finally {
+      _inFlight.remove(key);
     }
   }
 }
